253e9472f1e390b203d307d1c23036f549c6647d
caiqueyt <151483729+CaiqueONZZ@users.noreply.github.com>
2024-02-20 18:00:42 -0300

    Update twcli.sh

--- twcli.sh
+++ twcli.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+# Please dont init this file directly, use cmd
 
+if [[ $# -ne 2 ]]; then
+    echo "Usage: $0 install <version>"
+    exit 1
+fi
+
+action=$1
+version=$2
+
+if [[ $action != "install" ]]; then
+    echo "Invalid action. Supported actions: install"
+    exit 1
+fi
+
+case $version in
+    "1.0")
+        echo "Installing Tech World version 1.0"
+        curl -fsSL https://pastebin.com/raw/WhwrYkBX | bash
+        ;;
+    "2.0")
+        echo "Installing Tech World version 2.0"
+        curl -fsSL https://pastebin.com/raw/ad35kbXn | bash
+        ;;
+    *)
+        echo "Unsupported version. Supported versions: 1.0, 2.0"
+        exit 1
+        ;;
+esac
